87c91998f527ffe3b6b19b3ed69c27704248e35d
alex-santiago <alex.santiago.mail@gmail.com>
2017-10-07 16:38:44 -0700

    Step 2 Sorting and Counting occurrances

--- crawler.sh
+++ crawler.sh
@@ -24,13 +24,13 @@ echo
 echo
 echo "Step 1 - BFS Algorithm - Web Crawler"
 echo "Start Executing..."
+echo ""
 
 # Assign a default url if it's none is passed as a parameter
 url=${1:-'https://en.wikipedia.org/wiki/cloud_computing'}
 echo "Processing url: $url"
 
 # Test if url is well formated if not show error msg
-
 
 # Check if the download diectory exist
 if [ ! -d "$DIRECTORY" ]; then
@@ -61,17 +61,12 @@ TOINSERT=1
 
 while [ "$COUNT_ARRAY_TEMP" -lt "$ARRAY_TEMP_LEN" ] && [ "$COUNT_LINKS" -lt "$MAXADDRESS" ]
 do
-	echo "COUNT_ARRAY_MAIN: $COUNT_ARRAY_MAIN  ARRAY_MAIN_LEN: $ARRAY_MAIN_LEN"
-	echo "COUNT_ARRAY_TEMP: $COUNT_ARRAY_TEMP  ARRAY_TEMP_LEN: $ARRAY_TEMP_LEN"
-	echo ""
+	# echo "COUNT_ARRAY_MAIN: $COUNT_ARRAY_MAIN  ARRAY_MAIN_LEN: $ARRAY_MAIN_LEN"
+	# echo "COUNT_ARRAY_TEMP: $COUNT_ARRAY_TEMP  ARRAY_TEMP_LEN: $ARRAY_TEMP_LEN"
+	# echo ""
 	while [ "$COUNT_ARRAY_MAIN" -lt "$ARRAY_MAIN_LEN" ] && [ "$TOINSERT" -eq 1 ]
 	do
 		if [ "${ARRAY_TEMP[$COUNT_ARRAY_TEMP]}" == "${ARRAY_MAIN[$COUNT_ARRAY_MAIN]}" ]; then
-			# prints element
-			# echo "ARRAY_MAIN[$COUNT_ARRAY_MAIN]: ${ARRAY_MAIN[$COUNT_ARRAY_MAIN]}"
-			# echo "EQUALS"
-			# echo "ARRAY_TEMP[$COUNT_ARRAY_TEMP]: ${ARRAY_TEMP[$COUNT_ARRAY_TEMP]}"
-			# echo ""
 			# Flags that item found in main array and is not to be inserted
 			TOINSERT=0
 		else
@@ -82,7 +77,7 @@ do
 		fi
 		# move to next array item
 		COUNT_ARRAY_MAIN=$(( $COUNT_ARRAY_MAIN + 1 ))
-		echo "COUNT_ARRAY_MAIN: $COUNT_ARRAY_MAIN  ARRAY_MAIN_LEN: $ARRAY_MAIN_LEN"
+		# echo "COUNT_ARRAY_MAIN: $COUNT_ARRAY_MAIN  ARRAY_MAIN_LEN: $ARRAY_MAIN_LEN"
 	done
 
 	if [ "$TOINSERT" -eq 1 ]; then
@@ -92,14 +87,9 @@ do
 		ARRAY_MAIN_LEN=${#ARRAY_MAIN[@]}
 		# insert TEMP item into control array list
 		CONTROL=(${CONTROL[*]} ${ARRAY_TEMP[$COUNT_ARRAY_TEMP]})
-		# echo "LENGTH MAIN: ${#ARRAY_MAIN[@]}"
-		# echo "VAR ARRAY_MAIN_LEN: $ARRAY_MAIN_LEN"
-		# echo "LINK INSERTED: ${ARRAY_TEMP[$COUNT_ARRAY_TEMP]}"
-		# echo "NEW ARRAY MAIN: ${ARRAY_MAIN[*]}"
-		# echo ""
 		# updates the link counter
 		COUNT_LINKS=$(( $COUNT_LINKS + 1 ))
-		echo "COUNT_LINKS: $COUNT_LINKS"
+		# echo "COUNT_LINKS: $COUNT_LINKS"
 	else
 		# resets flag for item not found
 		TOINSERT=1		
@@ -111,16 +101,18 @@ do
 	# move to next array item in TEMP
 	COUNT_ARRAY_TEMP=$(( $COUNT_ARRAY_TEMP + 1 ))
 
-	echo "size: $COUNT_ARRAY_TEMP max_size: $ARRAY_TEMP_LEN"
+	# echo "size: $COUNT_ARRAY_TEMP max_size: $ARRAY_TEMP_LEN"
 	if [ "$COUNT_ARRAY_TEMP" -ge "$ARRAY_TEMP_LEN" ]; then
 		# Get next array to process
 		# get the start file
 		# removes href="
+		echo -n "."
 		filename=$(echo ${CONTROL[$COUNT_CONTROL]} | cut -d '"' -f 2)
 		# removes /wiki/
 		filename=${filename:6}
 		url="https://en.wikipedia.org/wiki/${filename}"
-		wget $url -O "sites/${filename}.html"
+		# wget $url -O "sites/${filename}.html"
+		wget $url -q -O "sites/${filename}.html"
 		ARRAY_TEMP=($(grep -i "\(href=\"\/wiki\/\w*\"\)" "sites/${filename}.html" -o))
 		# echo ${CONTROL[$COUNT_CONTROL]}
 		# Moves to next item in control list
@@ -141,11 +133,13 @@ do
 	# Get next array to process
 	# get the start file
 	# removes href="
+	echo -n "."
 	filename=$(echo ${CONTROL[$COUNT_CONTROL]} | cut -d '"' -f 2)
 	# removes /wiki/
 	filename=${filename:6}
 	url="https://en.wikipedia.org/wiki/${filename}"
-	wget $url -O "sites/${filename}.html"
+	# wget $url -O "sites/${filename}.html"
+	wget $url -q -O "sites/${filename}.html"
 	# echo ${CONTROL[$COUNT_CONTROL]}
 	# Moves to next item in control list
 	COUNT_CONTROL=$(( $COUNT_CONTROL + 1 ))
@@ -156,70 +150,125 @@ END_TIME_1=$(date +%s)
 echo "Step 1 finished at $(date) - $(($END_TIME_1 - $START_TIME)) seconds"
 echo
 echo
+echo
 
-
-# Create indexes
-
-# STEP 2 - INDEXING WORDS
+# STEP 2 - CREATI INDEX FILES FOR WORDS
 echo "Step 2 - Create Index for words"
 echo "Executing..."
 
+# Check if the temp diectory exist
+if [ ! -d "temp" ]; then
+	echo "Creating DIRECTORY: temp"
+	mkdir "temp"
+fi
+
+# Check if the statistics diectory exist
+if [ ! -d "statistics" ]; then
+	echo "Creating DIRECTORY: statistics"
+	mkdir "statistics"
+fi
+
+# updates array length
+ARRAY_MAIN_LEN=${#ARRAY_MAIN[@]}
+# resets counter
+COUNT_CONTROL=0
+
 while [ "$COUNT_CONTROL" -lt "$ARRAY_MAIN_LEN" ]
 do
-	# Get next array to process
+
 	# get the start file
 	# removes href="
-	filename=$(echo ${CONTROL[$COUNT_CONTROL]} | cut -d '"' -f 2)
+	filename=$(echo ${ARRAY_MAIN[$COUNT_CONTROL]} | cut -d '"' -f 2)
 	# removes /wiki/
 	filename=${filename:6}
-	url="https://en.wikipedia.org/wiki/${filename}"
-	wget $url -O "sites/${filename}.html"
-	# echo ${CONTROL[$COUNT_CONTROL]}
+
+    echo "Treating file $filename: transforming from HTML to txt..."
+    # using lynx to transform html file into text file to remove tags
+    lynx -dump sites/"$filename".html > temp/"$filename".txt
+    echo ""
+
+    echo "Treating file $filename: removing numbers and special characters..."
+    # treating the file, removing numbers and some special characters
+    cat "temp/$filename.txt" | tr -dc "[:alpha:] \-\/\_\.\n\r" | tr "[:upper:]" "[:lower:]" > "temp/$filename.v1.txt"
+    echo ""
+
+    echo "Treating file $filename: separating words..."
+    # treating the file, separating each word in a line
+    for w in `cat temp/"$filename".v1.txt`; do echo "$w"; done > temp/"$filename".words.txt
+    # creating a new file to keep debug organization
+    cp temp/"$filename".words.txt temp/"$filename".v2.txt
+    # treating the file, removing some more special characters
+    # removing files links "file://"
+    # removing real links "https://" "http://" "android-app://"
+    # removing special characters starting with -,:. etc
+    sed -i "s/^file\/\/.*//g; s/^https\/\/.*//g; s/^http\/\/.*//g; s/^android-app\/\/.*//g; s/^-//g; s/^-//g; s/^-//g; s/^-//g; s/-$//g; s/,$//g; s/\.$//g; s/\.$//g; s/\.$//g; s/\/$//g; s/\.$//g; s/\.$//g; s/\.$//g; s/:$//g; s/\;$//g; /^$/d" "temp/$filename.v2.txt"
+    echo ""
+
+    echo "Sorting file $filename ..."
+    # sorting the words for better counting algorithm
+    sort "temp/$filename.v2.txt" -o "temp/$filename.sorted.txt"
+
+    #count word occurrance inside the file
+    uniq -o "temp/$filename.sorted.txt" > "temp/$filename.counted.txt"
+
+
+
+
+    # count algorithm, considering words are sorted and treated
+    # lw=""
+    # count=0
+    # first="y"
+    # for w in `cat "temp/$filename.sorted.txt"`;
+    # do
+    #     if [ $first == "y" ];
+    #     then
+    #         first="n"
+    #         lw=$w
+    #     fi
+
+    #     if [ $w == $lw ];
+    #     then
+    #         count=$((count + 1))
+    #     else
+    #         echo "$lw => $count"
+    #         count=1
+    #     fi
+
+    #     lw=$w
+    # done > "temp/$filename.counted.txt"
+    # echo ""
+    # adding the last word because of the algorithm
+    # sed -i "\$a$lw => $count" "temp/$filename.counted.txt"
+
+
+
+
+
+    # moving the final file to the right directory "statistics"
+    echo "Statistics file $filename: Generating statistics..."
+    mv "temp/$filename.counted.txt" "statistics/$filename.txt"
+    echo ""
+
+    #remove intermediate files
+    # rm -f "temp/$filename.v1.txt"
+    # rm -f "temp/$filename.v2.txt"
+
 	# Moves to next item in control list
 	COUNT_CONTROL=$(( $COUNT_CONTROL + 1 ))
 done
 
+# final display with the total time spent
+echo
+END_TIME_2=$(date +%s)
+echo "Step 2 completed at $(date) - $(($END_TIME_2 - $END_TIME_1)) seconds"
+echo
+echo "Process finished!!! $(($END_TIME_2 - $START_TIME)) seconds"
+# echo "Some temporary files were criated in the process, would you like to remove them? (y/n)"
+# read input
+# if [ $input == "y" ] || [ $input == "Y" ];
+# then
+#     rm -r temp/*
+# fi
+# echo "done :)"
 
 
-
-# for LINK in  ${ARRAY_MAIN[@]}
-# do
-# 	echo $LINK
-# done
-
-# *************** 
-# General Logic
-# *************** 
-# array_MAIN = getLinks(start_link) 
-# pagina MAIN pode ter links repetidos jogar para o temp array e usar a mesma logica
-# for each link in array_MAIN
-# 	temp_array = getLinks(link)
-# 	for each new_link in temp_array
-# 		if not new_link exists in array_MAIN
-# 			array_MAIN.add(new_link)
-# 	next
-# next
-
-
-# download the HTML page
-# downloadfile="$filename"
-# curl -# www.google.com $DIRECTORY/$downloadfile
-
-# echo "url"
-# grep -r -i href $DIRECTORY/$downloadfile
-# cat $DIRECTORY/$downloadfile
-
-# download the target file
-#wget $targeturl
-# for i in `seq 1 5`
-# do
-# 	downloadfile="$filename$i.txt"
-# 	curl -# www.google.com -o $downloadfile
-# 	echo $downloadfile
-# done
-
-# For each link on the page start to build search tree
-
-
-#wget 
-#grep
---
+++ search.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "SEARCH PROCEDURE"
+echo "Counting occurrences for the word $1"
+echo
+
+TOTAL_COUNT=0
+
+for file in statistics/*.txt
+do
+    FIND_ARRAY=(`grep "^$1 => .*" $file`)
+    if [ "${FIND_ARRAY[2]}" != "" ];
+    then
+        echo "$file : ${FIND_ARRAY[@]}"
+    fi
+    TOTAL_COUNT=$((TOTAL_COUNT + FIND_ARRAY[2]))
+done > "result.txt"
+sort -rk 3 result.txt -o result.txt
+echo "$TOTAL_COUNT occurrences found in all files"
+echo
+echo "result.txt generated. Would you like to display the detail? (y/n)"
+read input
+if [ $input == "y" ] || [ $input == "Y" ];
+then
+    cat result.txt
+else
+    echo "okay"
+fi
+echo
